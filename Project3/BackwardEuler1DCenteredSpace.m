2f749f15da23882b5bfb808bbf79a17833c0359c
Harley <harleyhanes97@gmail.com>
2020-09-19 14:09:24 -0700

    Implemented Source Function for Finite Difference

--- Project3/BackwardEuler1DCenteredSpace.m
+++ Project3/BackwardEuler1DCenteredSpace.m
@@ -1,8 +1,13 @@
-function [uSol] = BackwardEuler1DCenteredSpace(x,t,uInit,D,bounds)
+function [uSol] = BackwardEuler1DCenteredSpace(x,t,uInit,D,bounds,varargin)
 %ForwardTime1DCenteredSpace Numerical PDE integrator for 1D 
 %diffusion with out forcing and with 0 boundary conditions
 %   Uses backward euler in time and centered difference in space
 %   D- diffusion term
+if nargin>6
+    error('Too many input arguments for BackwardEuler1DCenteredSpace')
+elseif nargin==6
+    sourceFunc=varargin{1};
+end
 
 
 %Check Inputs
@@ -10,8 +15,13 @@ function [uSol] = BackwardEuler1DCenteredSpace(x,t,uInit,D,bounds)
 %Define Method Varaibles
     %x- Recannotate to remove boundary values
     x=x(2:end-1);
+        %Make sure x is a column vec
+        if size(x,1)==1
+            x=x';
+            sprintf('Transitioned x from row to column vector')
+        end
     %uSol- Solution matrix where each column is a solution at ti
-    uSol=NaN(length(x),length(t));
+    uSol=NaN(length(x),1);
         uSol(:,1)=uInit(2:end-1);
     %deltaX
     deltaX=x(2)-x(1);
@@ -22,7 +32,7 @@ function [uSol] = BackwardEuler1DCenteredSpace(x,t,uInit,D,bounds)
     %deltaT
     deltaT=t(2)-t(1);
         %Confirm constant spacing in t
-        if sum(abs((t(2:end)-t(1:end-1))-deltaT))>10^(-12)
+        if max(abs((t(2:end)-t(1:end-1))-deltaT))>10^(-14)
             error('Spacing in x is not constant')
         end
     %A- Centered difference linear operator
@@ -32,9 +42,15 @@ function [uSol] = BackwardEuler1DCenteredSpace(x,t,uInit,D,bounds)
     A=diag(d1,0)+diag(d2,1)+diag(d2,-1);
     
 %Iterate over time
-for it=1:length(t)-1
-    uSol(:,it+1)=A\uSol(:,it);
+if nargin==6
+    for it=1:length(t)-1
+        uSol=A\(uSol+deltaT*sourceFunc(x,t(it+1)));
+    end
+else
+    for it=1:length(t)-1
+        uSol=A\uSol;
+    end
 end
-uSol=[bounds(1)*ones(1,length(t));uSol;bounds(2)*ones(1,length(t))];
+uSol=[bounds(1);uSol;bounds(2)];
 end
 
--- Project3/Project3main.m
+++ Project3/Project3main.m
@@ -14,7 +14,7 @@ set(0,'defaultLineLineWidth',4,'defaultAxesFontSize',20);
 %% Problem 3- Simulation Plot
     %Set up Simulation Parameters
             numSpacePoints=[11 21 31];
-            numTimePoints=[6 11 21];
+            numTimeSteps=[6 11 21];
     %Initialize Plot
             figure
             hold on
@@ -22,22 +22,23 @@ set(0,'defaultLineLineWidth',4,'defaultAxesFontSize',20);
     for iStep=1:length(numSpacePoints)
         %Calculate Simulation Variables
             xVec=linspace(0,2,numSpacePoints(iStep))';
-            tVec=linspace(0,1,numTimePoints(iStep))';
+            tVec=linspace(0,1,numTimeSteps(iStep))';
         %Get Numerical Solutions
-            uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.07,[0 0]);
+            uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.7,[0 0]);
         %Plot Numeric Solutions
             plot(xVec,uSol_Numeric(:,end))
             clear('xVec','tVec','uSol_numeric')
     end
     %Plot Analytic Solutions
     %Format Plot
+        title('$\frac{\partial u}{\partial t}=\alpha\frac{\partial^2 u}{\partial x^2}$','Interpreter','LaTex')
         xlabel('x','Interpreter','LaTex')
         ylabel('u(x,1)','Interpreter','Latex')
         %Make Legend,
         legendCell=cell(1,length(numSpacePoints)); %Stores legend strings
         for  iLegend=1:length(numSpacePoints)
             h=2/(numSpacePoints(iLegend)-1);
-            k=1/(numTimePoints(iLegend)-1);
+            k=1/(numTimeSteps(iLegend)-1);
             legendCell{iLegend}=sprintf('$\\hat{u}$ for $h=%.2g$, $k=%.2g$',h,k);
         end
         legend(legendCell,'Interpreter','Latex')
@@ -46,17 +47,17 @@ set(0,'defaultLineLineWidth',4,'defaultAxesFontSize',20);
 %% Problem 3- Convergence Tables
 clear;
 %Spatial Convergence
-dT=10^(-8);
-numSpacePoints=2*10^(-2).*[1 1/2 1/4 1/8 1/16];
-errorSpace=NaN(length(numSpacePoints),3);
-errorSpace(:,1)=numSpacePoints;
-for iStep=1:length(numSpacePoints)
+dT=10^(-5);
+dXsteps=(10^(-2).*[1 1/2 1/4 1/8 1/16]);
+errorSpace=NaN(length(dXsteps),3);
+errorSpace(:,1)=dXsteps;
+for iStep=1:length(dXsteps)
         %Calculate Simulation Variables
-            xVec=linspace(0,2,numSpacePoints(iStep))';
-            tVec=linspace(0,1,dT)';
+            xVec=(0:dXsteps(iStep):2)';
+            tVec=(0:dT:1)';
         %Get Analytic Solution
         %Get Numerical Solutions
-            uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.07,[0 0]);
+            uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.7,[0 0]);
         %Get Error
             errorSpace(2,iStep)=max(abs(uSol_Numeric-uSol_Analytic));
         %Caculate Relative Change in error
@@ -66,19 +67,19 @@ for iStep=1:length(numSpacePoints)
 end
 %Temporal Convergence
     %Get Spatial and Time Steps
-        dX=10^(-8);
+        dX=10^(-5);
             xVec=linspace(0,1,dT)';
-        numTimePoints=10^(-2).*[1 1/2 1/4 1/8 1/16];
+        dTsteps=10^(-2).*[1 1/2 1/4 1/8 1/16];
     %Setup Error Table
-        errorTime=NaN(length(numTimePoints),3);
-        errorTime(:,1)=numTimePoints;
+        errorTime=NaN(length(dTsteps),3);
+        errorTime(:,1)=dTsteps;
     %Get Analytic Solution
     %Get Numeric Solutions
     for iStep=1:length(numSpacePoints)
             %Calculate Simulation Variables
-                tVec=linspace(0,1,numTimePoints(iStep))';
+                tVec=linspace(0,1,dTsteps(iStep))';
             %Get Numerical Solutions
-                uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.07,[0 0]);
+                uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.7,[0 0]);
             %Get Error
                 errorTime(2,iStep)=max(abs(uSol_Numeric-uSol_Analytic));
             %Caculate Relative Change in error
@@ -94,6 +95,93 @@ end
         matrixToTexTable(errorSpace,SpaceRowNames,SpaceColNames,'filename','P3_SpaceErrorTable')
     %Time Table
         matrixToTexTable(errorTime,SpaceRowNames,SpaceColNames,'filename','P3_TimeErrorTable')
+        
+        
+        
+        
+        
 %% Problem 4
 
-%% Problem 5
+%% Problem 5- Simulation Plot
+    %Set up Simulation Parameters
+            numSpacePoints=[11 21 41];
+            numTimeSteps=[6 11 21 ];
+    %Initialize Plot
+            figure
+            hold on
+    %Loop over Step Values for plotting
+    for iStep=1:length(numSpacePoints)
+        %Calculate Simulation Variables
+            xVec=linspace(0,2,numSpacePoints(iStep))';
+            tVec=linspace(0,1,numTimeSteps(iStep))';
+        %Get Numerical Solutions
+            uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.7,[0 0],@(x,t)(x.*(x-2).*sin(3*pi*t)));
+        %Plot Numeric Solutions
+            plot(xVec,uSol_Numeric(:,end))
+            clear('xVec','tVec','uSol_numeric')
+    end
+    %Format Plot
+        title('$\frac{\partial u}{\partial t}=\alpha\frac{\partial^2 u}{\partial x^2}+x(x-2)sin(3\pi t)$','Interpreter','LaTex')
+        xlabel('x','Interpreter','LaTex')
+        ylabel('u(x,1)','Interpreter','Latex')
+        %Make Legend,
+        legendCell=cell(1,length(numSpacePoints)); %Stores legend strings
+        for  iLegend=1:length(numSpacePoints)
+            h=2/(numSpacePoints(iLegend)-1);
+            k=1/(numTimeSteps(iLegend)-1);
+            legendCell{iLegend}=sprintf('$\\hat{u}$ for $h=%.2g$, $k=%.2g$',h,k);
+        end
+        legend(legendCell,'Interpreter','Latex')
+        hold off
+        
+%% Problem 5- Convergence Tables
+clear;
+%Spatial Convergence
+dT=10^(-5);
+dXsteps=(10^(-2).*[1 1/2 1/4 1/8 1/16]);
+errorSpace=NaN(length(dXsteps),3);
+errorSpace(:,1)=dXsteps;
+for iStep=1:length(dXsteps)
+        %Calculate Simulation Variables
+            xVec=(0:dXsteps(iStep):2)';
+            tVec=(0:dT:1)';
+        %Get Analytic Solution
+        %Get Numerical Solutions
+            uSol_Numeric(:,1)=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.7,[0 0]);
+        %Get Error
+            errorSpace(2,iStep)=max(abs(uSol_Numeric-uSol_Analytic));
+        %Caculate Relative Change in error
+        if iStep~=1
+            errorSpace(3,iStep)=errorSpace(2,iStep-1)/errorSpace(2,iStep);
+        end
+end
+%Temporal Convergence
+    %Get Spatial and Time Steps
+        dX=10^(-5);
+            xVec=linspace(0,1,dT)';
+        dTsteps=10^(-2).*[1 1/2 1/4 1/8 1/16];
+    %Setup Error Table
+        errorTime=NaN(length(dTsteps),3);
+        errorTime(:,1)=dTsteps;
+    %Get Analytic Solution
+    %Get Numeric Solutions
+    for iStep=1:length(numSpacePoints)
+            %Calculate Simulation Variables
+                tVec=linspace(0,1,dTsteps(iStep))';
+            %Get Numerical Solutions
+                uSol_Numeric=BackwardEuler1DCenteredSpace(xVec,tVec,sin(pi*xVec/2),.7,[0 0]);
+            %Get Error
+                errorTime(2,iStep)=max(abs(uSol_Numeric-uSol_Analytic));
+            %Caculate Relative Change in error
+            if iStep~=1
+                errorTime(3,iStep)=errorTime(2,iStep-1)/errorTime(2,iStep);
+            end
+    end
+%Write Tables to LaTex
+    %Make Column and table names
+        SpaceColNames={'h','Error','$\frac{|E_i|}{|E_{i-1}|}$'};
+        SpaceRowNames=cell(0,0);
+    %Space Table
+        matrixToTexTable(errorSpace,SpaceRowNames,SpaceColNames,'filename','P3_SpaceErrorTable')
+    %Time Table
+        matrixToTexTable(errorTime,SpaceRowNames,SpaceColNames,'filename','P3_TimeErrorTable')
